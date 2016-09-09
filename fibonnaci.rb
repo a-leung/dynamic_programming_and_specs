c15650e275328b859a7cf7e6ea01e90d9c076fc2
Andrew Leung <andrew@ualberta.net>
2016-09-08 20:38:55 -0400

    dynamic programming applied

--- fibonnaci.rb
+++ fibonnaci.rb
@@ -1,9 +1,15 @@
 class Fibonnaci
+  def initialize
+    @cache = {}
+  end
+
   def num(at)
+    return @cache[at] if @cache[at]
+
     return 0 if at == 0
     return 1 if at == 1
     return 1 if at == 2
 
-    num(at-1) + num(at-2)
+    @cache[at] = num(at-1) + num(at-2)
   end
 end
